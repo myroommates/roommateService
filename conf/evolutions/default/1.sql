ed3e9c5ed34de2800edc7c3488eda76e795523b3
Florian <florian.jeanmart@factorx.eu>
2014-11-30 23:50:15 +0100

    fixe bug

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,22 +3,10 @@
 
 # --- !Ups
 
-create table category (
-  id                        bigint not null,
-  creation_date             timestamp not null,
-  last_update               timestamp not null,
-  name                      varchar(255) not null,
-  home_id                   bigint not null,
-  constraint uq_category_1 unique (name,home_id),
-  constraint pk_category primary key (id))
-;
-
 create table home (
   id                        bigint not null,
   creation_date             timestamp not null,
   last_update               timestamp not null,
-  name                      varchar(255) not null,
-  constraint uq_home_name unique (name),
   constraint pk_home primary key (id))
 ;
 
@@ -26,8 +14,7 @@ create table roommate (
   id                        bigint not null,
   creation_date             timestamp not null,
   last_update               timestamp not null,
-  first_name                varchar(255) not null,
-  last_name                 varchar(255) not null,
+  name                      varchar(255) not null,
   email                     varchar(255) not null,
   password                  varchar(255) not null,
   home_id                   bigint not null,
@@ -44,7 +31,7 @@ create table ticket (
   last_update               timestamp not null,
   description               Text not null,
   value                     float not null,
-  category_id               bigint,
+  category                  varchar(255),
   date                      timestamp not null,
   home_id                   bigint not null,
   creator_id                bigint not null,
@@ -57,24 +44,18 @@ create table mm_ticket_prayers (
   roommate_id                    bigint not null,
   constraint pk_mm_ticket_prayers primary key (ticket_id, roommate_id))
 ;
-create sequence category_seq;
-
 create sequence home_seq;
 
 create sequence roommate_seq;
 
 create sequence ticket_seq;
 
-alter table category add constraint fk_category_home_1 foreign key (home_id) references home (id);
-create index ix_category_home_1 on category (home_id);
-alter table roommate add constraint fk_roommate_home_2 foreign key (home_id) references home (id);
-create index ix_roommate_home_2 on roommate (home_id);
-alter table ticket add constraint fk_ticket_category_3 foreign key (category_id) references category (id);
-create index ix_ticket_category_3 on ticket (category_id);
-alter table ticket add constraint fk_ticket_home_4 foreign key (home_id) references home (id);
-create index ix_ticket_home_4 on ticket (home_id);
-alter table ticket add constraint fk_ticket_creator_5 foreign key (creator_id) references roommate (id);
-create index ix_ticket_creator_5 on ticket (creator_id);
+alter table roommate add constraint fk_roommate_home_1 foreign key (home_id) references home (id);
+create index ix_roommate_home_1 on roommate (home_id);
+alter table ticket add constraint fk_ticket_home_2 foreign key (home_id) references home (id);
+create index ix_ticket_home_2 on ticket (home_id);
+alter table ticket add constraint fk_ticket_creator_3 foreign key (creator_id) references roommate (id);
+create index ix_ticket_creator_3 on ticket (creator_id);
 
 
 
@@ -84,8 +65,6 @@ alter table mm_ticket_prayers add constraint fk_mm_ticket_prayers_roommate_02 fo
 
 # --- !Downs
 
-drop table if exists category cascade;
-
 drop table if exists home cascade;
 
 drop table if exists roommate cascade;
@@ -93,8 +72,6 @@ drop table if exists roommate cascade;
 drop table if exists ticket cascade;
 
 drop table if exists mm_ticket_prayers cascade;
-
-drop sequence if exists category_seq;
 
 drop sequence if exists home_seq;
 
