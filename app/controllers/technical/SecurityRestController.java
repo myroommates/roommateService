809acfe4f44e922611ad50ed5c8a2ef33631c2e7
Florian <florian.jeanmart@factorx.eu>
2015-01-14 19:34:23 +0100

    r

--- app/controllers/technical/SecurityRestController.java
+++ app/controllers/technical/SecurityRestController.java
@@ -1,7 +1,11 @@
 package controllers.technical;
 
+import dto.technical.ExceptionDTO;
+import play.api.i18n.Lang;
+import play.i18n.Messages;
 import play.mvc.Http;
 import play.mvc.Result;
+import util.ErrorMessage;
 
 /**
  * Created by florian on 10/11/14.
@@ -10,6 +14,6 @@ public class SecurityRestController extends CommonSecurityController {
 
     @Override
     public Result onUnauthorized(Http.Context ctx) {
-        return unauthorized();
+        return unauthorized(new ExceptionDTO(Messages.get(Lang.defaultLang(),ErrorMessage.NOT_CONNECTED.name())));
     }
 }
