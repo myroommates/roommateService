ab616ec4b3a2a8fd803156a605620713e40e2cd8
florian <florian.jeanmart@factorx.eu>
2015-05-02 18:49:40 +0200

    e

---
+++ app/controllers/TestController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import com.avaje.ebean.annotation.Transactional;
+import controllers.technical.AbstractController;
+import controllers.technical.SecurityController;
+import converter.RoommateToInterfaceDataDTOConverter;
+import converter.RoommateToRoommateDTOConverter;
+import play.mvc.Result;
+import play.mvc.Security;
+import services.RoommateService;
+import services.ShoppingItemService;
+import services.TicketDebtorService;
+import services.TicketService;
+import services.impl.RoommateServiceImpl;
+import services.impl.ShoppingItemServiceImpl;
+import services.impl.TicketDebtorServiceImpl;
+import services.impl.TicketServiceImpl;
+
+/**
+ * Created by florian on 2/05/15.
+ */
+public class TestController  extends AbstractController {
+
+    @Security.Authenticated(SecurityController.class)
+    @Transactional
+    public Result index() {
+
+
+        return ok(views.html.test.render());
+    }
+}
